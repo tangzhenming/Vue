3a226dec3fba702832ab2d85791db51ef6da3184
tang <15977536060@163.com>
2022-06-02 17:38:50 +0800

    使用 vue-router

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
+<!-- 
+  router-link 和 router-view 是由 vue-router 注册的全局组件
+  router-link 负责跳转不同的页面，相当于 Vue 世界中的超链接 a 标签
+  router-view 负责渲染路由匹配的组件
+  我们可以通过把 router-view 放在不同的地方，实现复杂项目的页面布局
+ -->
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
 </template>
 
 <style>
